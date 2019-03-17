6ee1771076b835762225486524874bea1a9665c1
gookcompsci <gookcompsci@hotmail.com>
2019-03-18 00:29:16 +0700

    chk

---
+++ jobworker/lib/queue-scheduler.js
@@ -0,0 +1,14 @@
+module.exports.create = function (prm)
+{
+    return new QS(prm);
+}
+
+var QS = function (prm)
+{
+    this.gen_proc_queue = [];
+}
+
+QS.prototype.proc_enqueue = function (prm)
+{
+
+}
